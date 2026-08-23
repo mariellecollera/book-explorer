0e933aeeb51f2a6b4d5b052e8567b7ada0a75bb2
mariellecollera <marielle_anne_collera@dlsl.edu.ph>
2026-08-23 11:45:59 +0800

    fix: move Add a book button inside the MyUmbrella grid

--- src/pages/MyUmbrella.jsx
+++ src/pages/MyUmbrella.jsx
@@ -24,23 +24,22 @@ export default function MyUmbrella() {
         <p className="text-black italic text-[16px]">
           View all books under your umbrella.
         </p>
-
-        <button
-          className="button-black mt-6"
-          onClick={() => navigate("/results")}
-        >
-          + Add a book
-        </button>
       </div>
 
       <div className="w-full border border-black p-6">
+        <div className="flex justify-end mb-5">
+          <button className="button-black" onClick={() => navigate("/results")}>
+            + Add a book
+          </button>
+        </div>
+
         {books.length === 0 ? (
           <div className="py-8 flex flex-col items-center justify-center italic text-[18px]">
             <img src={empty} alt="No books saved" className="w-30 h-30 mb-2" />
             Your Umbrella is empty.
           </div>
         ) : (
-          <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 gap-x-2 gap-y-4 items-start place-items-center">
+          <div className="bg-[pink] grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 gap-4 items-start place-items-center items-center">
             {books.map((b) => (
               <div key={b.id} className="relative">
                 <ResultsCard
