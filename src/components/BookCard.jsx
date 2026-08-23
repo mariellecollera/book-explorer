ba1193edc6a47791588e898dc7e0cf22b51de84a
mariellecollera <marielle_anne_collera@dlsl.edu.ph>
2026-08-23 22:12:21 +0800

    feat: add hover animation to book cards

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -15,7 +15,7 @@ export default function BookCard({
 
   return (
     <div
-      className="flex flex-col items-center cursor-pointer"
+      className="flex flex-col items-center cursor-pointer duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-102"
       onClick={onSelect}
       role="button"
       tabIndex={0}
